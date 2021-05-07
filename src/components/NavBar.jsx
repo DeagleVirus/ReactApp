89f2c75199dfb0e740bf822db820172db81214a0
Alex <developerment975@gmail.com>
2021-05-07 19:52:21 +0300

    first commit on my react.js project

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+const NavBar = () => {
+    return (
+        <nav className="navigation">
+          <div>
+            Home
+          </div>
+          <div>
+            My profile
+            </div>
+          <div>
+            Messages
+            </div>
+          <div>
+            Friends
+            </div>
+          <div>
+            Settings
+          </div>
+      </nav>
+    );
+}
+
+export default NavBar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+const Profile = () => {
+    return (
+        <div className="content">
+          <img src="https://i.pinimg.com/originals/4d/b0/dd/4db0ddad2581a98832b486d6ff9788e2.jpg" />
+          <div>
+            avatar
+          </div>
+
+          <div>
+            profile description
+          </div>
+
+          <div>
+            My posts
+          </div>
+
+          <div>
+            new post
+          </div>
+
+          <div>
+            post1
+          </div>
+
+          <div>
+            post2
+          </div>
+      </div>
+    );
+}
+
+export default Profile;
